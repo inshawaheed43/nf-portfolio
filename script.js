80ca0ac7a91594c2514264c3d96d13cc89bcd6e9
Insha Waheed <waheedinsha02@gmail.com>
2025-04-20 21:25:23 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+// gsap.to("body",{
+//    scrollTrigger:{
+//     scrub: 2,
+//    }
+// })
+
+
+// gsap.to("#page1 #logo",{
+   
+// })
+
+// gsap.to("#page1 #person-img", {
+//     y: 10,
+
+// })
+
+// var tl = gsap.timeline()
+
+// tl.to("#page1 #logo", {
+//     x: 50,
+//     duration: 1,
+    
+// })
+
+// tl.to("h6", {
+//     x: -20,
+//     duration: 1.5,
+//     scale: 0.5,
+// })
+// // tl.to("#box2", {
+// //     x:900,
+// //     duration: 1.5,
+// // })
+
+// gsap.to("#page1 h4")
